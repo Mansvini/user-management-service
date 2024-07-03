85583bc60d8bcf22a52a85e5e6782b4174d803c0
Mansvini Aggarwal <coderspecial@gmail.com>
2024-07-03 18:01:01 +0530

    User CRUD operations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() user: User): Promise<any> {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.userService.remove(id);
+  }
+}
