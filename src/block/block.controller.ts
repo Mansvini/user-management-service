9da517b674b8f225882e89ca624ede4e1ae2138c
Mansvini Aggarwal <coderspecial@gmail.com>
2024-07-04 22:17:51 +0530

    Updating block controller with user id from JWT token

--- src/block/block.controller.ts
+++ src/block/block.controller.ts
@@ -1,23 +1,18 @@
 import { Controller, Post, Delete, Param } from '@nestjs/common';
 import { BlockService } from './block.service';
+import { UserId } from '../common/user-id.decorator';
 
 @Controller('block')
 export class BlockController {
   constructor(private readonly blockService: BlockService) {}
 
-  @Post(':blockerId/:blockedId')
-  block(
-    @Param('blockerId') blockerId: number,
-    @Param('blockedId') blockedId: number,
-  ) {
+  @Post(':blockedId')
+  block(@UserId() blockerId: number, @Param('blockedId') blockedId: number) {
     return this.blockService.blockUser(blockerId, blockedId);
   }
 
-  @Delete(':blockerId/:blockedId')
-  unblock(
-    @Param('blockerId') blockerId: number,
-    @Param('blockedId') blockedId: number,
-  ) {
+  @Delete(':blockedId')
+  unblock(@UserId() blockerId: number, @Param('blockedId') blockedId: number) {
     return this.blockService.unblockUser(blockerId, blockedId);
   }
 }
